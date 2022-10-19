5b2713f8627eea9c0c2650bd376b9d2c9dbdc2cb
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2022-10-19 12:56:07 -0500

    Segundo cambio de texto en src, bin y readme

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola Mundo, Intento de hacer Pull Push");
+        System.out.println("Segundo texto, directo en la rama Master");
     }
 }
